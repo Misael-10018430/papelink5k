d4dc7db9ef73f5c4191875c734477e1695698171
MISAEL L.A. <misa690la90@outlook.com>
2025-10-27 14:35:03 -0600

    cambios

--- View/cliente/index.php
+++ View/cliente/index.php
@@ -784,7 +784,7 @@ include 'includes/header.php';
                 $imagenUrl = $imagenesCategoria[$nombreCat] ?? 'https://images.unsplash.com/photo-1586023492125-27b2c045efd7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80';
             ?>
                 <div class="card-categoria">
-                    <a href="productos.php?categoria=<?php echo $categoria['IdCategoria']; ?>">
+                    <a href=" categorias.php?categoria=<?php echo $categoria['IdCategoria']; ?>">
                         <img src="<?php echo $imagenUrl; ?>" 
                              alt="<?php echo htmlspecialchars($nombreCat); ?>">
                         
